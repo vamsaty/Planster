fd521377bd021bb84fcf5eccafc0aba4fd423d9f
vansay <satyspy007@gmail.com>
2019-10-10 21:11:23 +0530

    updated app directory

--- app/src/Containers/App.js
+++ app/src/Containers/App.js
@@ -8,10 +8,12 @@ class App extends Component{
   
   render(){
 
-
+  
     return (
       <div>
+        <h1> SE Project Begins </h1>
         <img src={logo} className="App-logo" alt="logo" />
+        
       </div>
     );
   }
