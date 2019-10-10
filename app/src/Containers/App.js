1631873c178163e1ddb75c4153abe1f53406003f
vansay <satyspy007@gmail.com>
2019-10-10 21:02:49 +0530

    uploaded app directory

---
+++ app/src/Containers/App.js
@@ -0,0 +1,20 @@
+import React,{Component} from 'react';
+import logo from '../logo.svg';
+import './App.css';
+
+
+class App extends Component{
+  
+  
+  render(){
+
+
+    return (
+      <div>
+        <img src={logo} className="App-logo" alt="logo" />
+      </div>
+    );
+  }
+}
+
+export default App;
