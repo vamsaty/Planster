bc35d11c4d4a90e051bf7cdb5ccc06b7a1a4cfe0
shivangi-jadon <shivangijadon16@gmail.com>
2019-11-14 19:05:33 +0530

    .

--- app/src/components/Dashboard/Track.js
+++ app/src/components/Dashboard/Track.js
@@ -1,17 +1,11 @@
-import React,{ useState,Component } from 'react'
-import ReactMapGL, {LinearInterpolator, FlyToInterpolator} from 'react-map-gl';
-import MapGL, {NavigationControl, Marker,Popup} from 'react-map-gl';
-import { Icon } from 'semantic-ui-react';
+import React,{ Component } from 'react'
+import ReactMapGL from 'react-map-gl';
+import  {NavigationControl, Marker,Popup} from 'react-map-gl';
 import pointer from '../../assets/images/clocation.png'
 import friend from '../../assets/images/flocation.png'
 import axios from "axios";
 const TOKEN="pk.eyJ1Ijoic2phZG9uIiwiYSI6ImNrMnZ2dDhjajA4cGkzZHBnNGJwdGx3eGoifQ.cochyrkRr-f87PZXJCo4Ww"
 
-const geolocateStyle = {
-  float: 'left',
-  margin: '50px',
-  padding: '10px'
-};
 
 const navStyle = {
   position: 'absolute',
@@ -20,22 +14,11 @@ const navStyle = {
   padding: '10px'
 };
 
-const markerList=[
-    {lat: 17.441013, 
-    long: 78.391796,
-    info: 10},
-    { lat:17.442889, 
-    long: 78.396073,
-    info: 20},
-    {lat: 17.441681, 
-    long: 78.394357,
-    info: 10}
-    ];
+
 class Track extends Component{
 
   constructor(props) {
     super(props)
-  
     this.state = {
       viewport: {
           width: 950,
@@ -48,50 +31,17 @@ class Track extends Component{
       friends_coordinates:[],
       l:0,
       lo:0,
-      index:-1,
   }
-    
     this.trackLocation=this.trackLocation.bind(this)
-    this.renderPopup=this.renderPopup.bind(this)
     this.display=this.display.bind(this)
   }
 
-  
 
-findCoordinates = () => {
-  navigator.geolocation.getCurrentPosition(
-    position => {
-      const location = JSON.stringify(position);
-      console.log(position)
-      let lat=position.coords.latitude
-      let long =position.coords.longitude
-        this.setState({viewport:{...this.state.viewport,latitude:position.coords.latitude,longitude:position.coords.longitude}});
-      
-
-    },
-    error => alert(error.message),
-    { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
-  );
-};
 _onViewportChange = viewport => {
     this.setState({viewport});
 };
 
-setpopup = () => {
-  this.setState({popupInfo: true})
-}
-
-delpopup = () => {
-  this.setState({popupInfo: null})
-}
-
-renderPopup =(lat,long)=>{
-  return(
-    alert("gg")
-    )
-}
-
- trackLocation()
+trackLocation()
   {
     navigator.geolocation.getCurrentPosition(
       position => {
@@ -99,25 +49,17 @@ renderPopup =(lat,long)=>{
         console.log(position)
         let lat=position.coords.latitude
         let long =position.coords.longitude
-      this.setState({viewport:{...this.state.viewport,latitude:position.coords.latitude,
+        this.setState({viewport:{...this.state.viewport,latitude:position.coords.latitude,
         longitude:position.coords.longitude}})
-        
-  
       },
       error => alert(error.message),
       { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
     );
-    
     axios.post('http://localhost:5000/api/v1/setlocation/'+String(sessionStorage.getItem("Name")),
       {'latitude':this.state.viewport.latitude ,'longitude':this.state.viewport.longitude })
-      .then( response => {
-        })
-        .catch(error => {
-         
-        });
-
+      .then( response => {})
+      .catch(error => {});
         var i;
-        
         var coordinates=[]
         for(i=0;i<this.props.members.length;i++)
         {
@@ -125,37 +67,21 @@ renderPopup =(lat,long)=>{
           if(this.props.members[i]==String(sessionStorage.getItem("Name")))
           continue;
           axios.get('http://localhost:5000/api/v1/getlocation/'+this.props.members[i])
-      .then( response => {
-        
-        const min = 0.01;
-        const max = 0.001;
-        const rand = min + Math.random() * (max - min);
-        x.push(response.data.latitude)
-        x.push(response.data.longitude)
-        x.push(response.data.name)
-        coordinates.push(x)
-      //  console.log(coordinates)
-      this.setState({l:response.data.latitude+rand,lo:response.data.longitude-rand})
-        
-        })
-        
-        .catch(error => {
-         
-        });
-      }
-      
-      this.setState({friends_coordinates:coordinates})
-    
-        console.log(this.state.friends_coordinates)
-
-    
-        //console.log(friends_longitudes)
-       
+          .then( response => {
+            x.push(response.data.latitude)
+            x.push(response.data.longitude)
+            x.push(response.data.name)
+            coordinates.push(x)
+            //  console.log(coordinates)
+            this.setState({l:response.data.latitude,lo:response.data.longitude})    })
+          .catch(error => {});
+        }
+      this.setState({friends_coordinates:coordinates}) 
   }
+
 componentDidMount()
 {
   this.trackLocation()
-  //this.getLocation();
   setInterval(this.trackLocation,3000)
 }
 
@@ -165,6 +91,7 @@ display = () =>
   var d=document.getElementById(this.state.index)
   d.style.display="block"
 }
+
 
 distance(lat1, lon1, lat2, lon2, unit) {
 	if ((lat1 == lat2) && (lon1 == lon2)) {
@@ -188,39 +115,37 @@ distance(lat1, lon1, lat2, lon2, unit) {
 	}
 }
 
-render() {
-
-  
+render() {  
     return (
         <div>
-            <ReactMapGL
+          <ReactMapGL
             {...this.state.viewport} onViewportChange={this._onViewportChange}
             mapStyle="mapbox://styles/mapbox/streets-v11"
             mapboxApiAccessToken={TOKEN} >
             <div className="nav" style={navStyle}>
           <NavigationControl onViewportChange={this._onViewportChange}/>
           <div>
-          <Marker latitude={this.state.viewport.latitude} longitude={this.state.viewport.longitude} offsetLeft={-20} offsetTop={-10}>
-          <img style={{width:"1.8em"}} src={pointer} />
-        </Marker></div>
-        {this.state.friends_coordinates.map( (val, ind) => (
+            <Marker latitude={this.state.viewport.latitude} longitude={this.state.viewport.longitude} offsetLeft={-20} offsetTop={-10}>
+            <img style={{width:"1.8em"}} src={pointer} />
+            </Marker>
+          </div>
+          {this.state.friends_coordinates.map( (val, ind) => (
           <div key={ind}>
           <Marker latitude={val[0]} longitude={val[1]} offsetLeft={-20} offsetTop={-10}>
           <img style={{width:"1.8em"}} src={friend} onMouseEnter={()=>{this.setState({index:ind})}} onClick={this.display}/>
-        </Marker><div id={ind} style={{display:"none"}}><Popup tipSize={5}
-        anchor="bottom-right"
-        longitude={val[1]}
-        latitude={val[0]}
-        closeButton={false} closeOnClick={false}
-        height="10px">
-        <p>{val[2]} {this.distance(this.state.viewport.latitude,this.state.viewport.longitude,val[0],val[1],'K')}km</p>
-      </Popup></div></div>
-        ))}
-        
-              
+          </Marker>
+          <div id={ind} style={{display:"none"}}>
+          <Popup tipSize={5}
+          anchor="bottom-right"
+          longitude={val[1]}
+          latitude={val[0]}
+          closeButton={false} closeOnClick={false}
+          height="10px">
+          <p>{val[2]} {this.distance(this.state.viewport.latitude,this.state.viewport.longitude,val[0],val[1],'K')}km</p>
+          </Popup></div></div>
+        ))}    
        </div>
           </ReactMapGL>
-          
         </div>
     );
 }
